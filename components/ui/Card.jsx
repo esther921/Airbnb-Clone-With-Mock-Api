515683dcc3cd409c1e316b2cbd8c55dcc23cde72
esther921 <estherniyonkuru51@gmail.com>
2023-01-04 08:35:12 +0200

    fix:testing the errors on the deployment

--- components/ui/Card.jsx
+++
@@ -1,7 +0,0 @@
-import React from "react";
-
-const ImageCard = () => {
-  return <div className={`min-w-[19.3125rem] `}>Card</div>;
-};
-
-export default ImageCard;
---
+++ components/ui/ImageCard.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import Image from "next/image";
+
+const ImageCard = () => {
+  return (
+    <div className={`min-w-[19.3125rem] `}>
+      <Image alt="" src="" />
+    </div>
+  );
+};
+
+export default ImageCard;
